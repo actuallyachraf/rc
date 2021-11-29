a8b3984c2110560750552f5374074d21bde27e8c
actuallyachraf <actuallyachraf@gmail.com>
2021-11-29 15:33:43 +0100

    feat:idiomatize code and use more efficient structs

--- src/lex/mod.rs
+++ src/lex/mod.rs
@@ -13,26 +13,26 @@ pub struct Lexer {
 
 #[allow(dead_code)]
 impl Lexer {
-    pub fn new(s: String) -> Lexer {
-        let chars: Vec<char> = s.chars().collect();
+    pub fn new(input: String) -> Lexer {
+        let chars: Vec<char> = input.chars().collect();
         let curr = '0';
         let next = '0';
         let read_pos = 0;
         let pos = 0;
         let tokens = Vec::<token::Token>::new();
         let mut l = Lexer {
-            input: s,
-            chars: chars,
-            curr: curr,
-            next: next,
-            pos: pos,
-            read_pos: read_pos,
-            tokens: tokens,
+            input,
+            chars,
+            curr,
+            next,
+            pos,
+            read_pos,
+            tokens,
         };
-        l.read_char();
+        l.next_char();
         l
     }
-    pub fn read_char(&mut self) {
+    pub fn next_char(&mut self) {
         if self.read_pos >= self.input.len() {
             // signal EOF
             self.curr = '0';
@@ -40,15 +40,15 @@ impl Lexer {
             self.curr = self.chars[self.read_pos];
         }
         self.pos = self.read_pos;
-        self.read_pos = self.read_pos + 1;
+        self.read_pos += 1;
     }
-    pub fn peek_char(&mut self) -> char {
+    pub fn peek(&mut self) -> char {
         if self.read_pos >= self.input.len() {
             return 0 as char;
         }
-        return self.chars[self.read_pos];
+        self.chars[self.read_pos]
     }
-    pub fn next_token(&mut self) -> token::Token {
+    pub fn next(&mut self) -> token::Token {
         match self.curr {
             ',' => token::Token::Comma,
             ';' => token::Token::SemiColon,
@@ -59,11 +59,11 @@ impl Lexer {
             '}' => token::Token::RBrace,
             '{' => token::Token::LBrace,
             '=' => {
-                if self.peek_char() == '=' {
-                    self.read_char();
-                    return token::Token::Equal;
+                if self.peek() == '=' {
+                    self.next_char();
+                    token::Token::Equal
                 } else {
-                    return token::Token::Assign;
+                    token::Token::Assign
                 }
             }
             _ => token::Token::EOF,
@@ -71,9 +71,7 @@ impl Lexer {
     }
 
     pub fn lex(&mut self) {
-        for c in self.chars.clone() {
-            println!("Char : {}", c);
-            println!("Current : {}", self.curr);
+        for _ in 0..self.chars.len() {
             match self.curr {
                 ',' => self.tokens.push(token::Token::Comma),
                 ';' => self.tokens.push(token::Token::SemiColon),
@@ -84,16 +82,16 @@ impl Lexer {
                 '}' => self.tokens.push(token::Token::RBrace),
                 '{' => self.tokens.push(token::Token::LBrace),
                 '=' => {
-                    if self.peek_char() == '=' {
+                    if self.peek() == '=' {
                         self.tokens.push(token::Token::Equal);
-                        self.read_char();
+                        self.next_char();
                     } else {
                         self.tokens.push(token::Token::Assign)
                     }
                 }
                 _ => return,
             }
-            self.read_char()
+            self.next_char()
         }
     }
     pub fn tokens(&self) -> Vec<token::Token> {
@@ -115,6 +113,6 @@ mod tests {
         ];
         let mut lexer = Lexer::new(String::from(",=.{=="));
         lexer.lex();
-        assert_eq!(expected == lexer.tokens(), true);
+        assert!(expected == lexer.tokens());
     }
 }
